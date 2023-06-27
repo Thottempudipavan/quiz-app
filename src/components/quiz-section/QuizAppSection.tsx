df809fe12d3bd6da06dd3fcb668de742d59dd681
Pavan Kumar <pkumar07071990@gmail.com>
2023-06-28 01:25:37 +0530

    chnages

--- src/components/quiz-section/QuizAppSection.tsx
+++ src/components/quiz-section/QuizAppSection.tsx
@@ -31,12 +31,12 @@ const QuizAppSection = () => {
 
   const handleClick = async () => {
     try {
-      // const { data } = await fetch(
-      //   "https://pkumar-07071990-hlmrnfk6ta-uc.a.run.app/getquestions"
-      // ).then((res) => res.json());
       const { data } = await fetch(
-        `http://localhost:8080/getquestions?count=${selectionCount}`
+        `https://https://pkumar-07071990-hlmrnfk6ta-uc.a.run.app/getquestions?count=${selectionCount}`
       ).then((res) => res.json());
+      // const { data } = await fetch(
+      //   `http://localhost:8080/getquestions?count=${selectionCount}`
+      // ).then((res) => res.json());
       setquizItems({ data: data, loading: false });
     } catch (_err) {
       const selectedArray: any[] = questions.filter(
